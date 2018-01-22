5b23fa48c5a56540bcac03dffa1e963a121f6aa2
h3llrais3r <pooh_beer_1@hotmail.com>
2018-01-22 12:50:55 +0100

    Rename firebase db references

--- src/app/chat/chat.service.ts
+++ src/app/chat/chat.service.ts
@@ -8,12 +8,15 @@ import { StringFormat } from '../shared/util/string-format';
 @Injectable()
 export class ChatService {
 
-  private CHATBOX_CHAT_REF = '/chatbox/chats/';
-  private CHATROOM_LIST_REF = '/chatrooms/lists/';
-  private CHATROOM_LIST_CHATROOM_REF = '/chatrooms/lists/{0}/';
-  private CHATROOM_REF = '/chatrooms/{0}/';
-  private CHATROOM_CHAT_REF = '/chatrooms/{0}/chats/';
-  private CHATROOM_TYPING_REF = '/chatrooms/{0}/typing/';
+  private CHATBOX_REF = '/chatbox/';
+  private CHATBOX_CHATS_REF = '/chatbox/chats/';
+  private CHATBOX_TYPING_REF = '/chatbox/typing/';
+  private CHATROOMS_REF = '/chatrooms/';
+  private CHATROOMS_LIST_REF = '/chatrooms/list/';
+  private CHATROOMS_LIST_CHATROOM_REF = '/chatrooms/lists/{0}/';
+  private CHATROOMS_CHATROOM_REF = '/chatrooms/{0}/';
+  private CHATROOMS_CHATROOM_CHATS_REF = '/chatrooms/{0}/chats/';
+  private CHATROOMS_CHATROOM_TYPING_REF = '/chatrooms/{0}/typing/';
 
   constructor(private db: AngularFireDatabase, private storeService: StoreService) { }
 
@@ -32,7 +35,7 @@ export class ChatService {
 
   private activateChatRoom(uuid: string): ChatRoom {
     // Ativate chatroom
-    let chatRoomReference = this.db.database.ref(StringFormat.format(this.CHATROOM_LIST_CHATROOM_REF, uuid));
+    let chatRoomReference = this.db.database.ref(StringFormat.format(this.CHATROOMS_LIST_CHATROOM_REF, uuid));
     let chatRoom = new ChatRoom(uuid);
     chatRoomReference.update(chatRoom);
     // Handle disconnect of chatroom
@@ -42,43 +45,43 @@ export class ChatService {
 
   private createChatRoom(): ChatRoom {
     // Create chatroom (we use the key from firebase as uuid of the chat)
-    let chatRoomReference = this.db.database.ref(this.CHATROOM_LIST_REF).push();
+    let chatRoomReference = this.db.database.ref(this.CHATROOMS_LIST_REF).push();
     let uuid = chatRoomReference.key;
     let chatRoom = new ChatRoom(uuid);
     chatRoomReference.set(chatRoom);
     // Handle disconnect of chatroom
     chatRoomReference.onDisconnect().update({ active: false });
     // Push welcome message
-    this.db.database.ref(StringFormat.format(this.CHATROOM_CHAT_REF, uuid)).push(new Chat('', new Date(), 'Welcome to chatroom ' + uuid));
+    this.db.database.ref(StringFormat.format(this.CHATROOMS_CHATROOM_CHATS_REF, uuid)).push(new Chat('', new Date(), 'Welcome to chatroom ' + uuid));
     return chatRoom;
   }
 
   getChatRooms(active: boolean = false): AngularFireList<ChatRoom> {
     // We keep a list of chatroom uuids to prevent the loading of all chatrooms
     if (active) {
-      return this.db.list(this.CHATROOM_LIST_REF, ref => ref.orderByChild('active').equalTo(true));
+      return this.db.list(this.CHATROOMS_LIST_REF, ref => ref.orderByChild('active').equalTo(true));
     } else {
-      return this.db.list(this.CHATROOM_LIST_REF);
+      return this.db.list(this.CHATROOMS_LIST_REF);
     }
   }
 
   getChats(chatRoom: ChatRoom = null): AngularFireList<Chat> {
     if (chatRoom) {
       console.log('Getting chats from chatroom ' + chatRoom.uuid + '...');
-      return this.db.list(StringFormat.format(this.CHATROOM_CHAT_REF, chatRoom.uuid));
+      return this.db.list(StringFormat.format(this.CHATROOMS_CHATROOM_CHATS_REF, chatRoom.uuid));
     } else {
       console.log('Getting chats from chatbox...')
-      return this.db.list(this.CHATBOX_CHAT_REF);
+      return this.db.list(this.CHATBOX_CHATS_REF);
     }
   }
 
   submitChat(chat: Chat, chatRoom: ChatRoom = null): void {
     if (chatRoom) {
       console.log('Submitting chat to chatroom ' + chatRoom.uuid + '...');
-      this.db.list(StringFormat.format(this.CHATROOM_CHAT_REF, chatRoom.uuid)).push(chat);
+      this.db.list(StringFormat.format(this.CHATROOMS_CHATROOM_CHATS_REF, chatRoom.uuid)).push(chat);
     } else {
       console.log('Submitting chat to chatbox...')
-      this.db.list(this.CHATBOX_CHAT_REF).push(chat);
+      this.db.list(this.CHATBOX_CHATS_REF).push(chat);
     }
   }
 
